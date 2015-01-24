508498af35a79ff8b4f9a82878bc53388d321fd2
Marco Montanari <sirmmo@gmail.com>
2015-01-24 11:44:48 +0100

    updates

--- export_v1.py
+++ export_v1.py
@@ -2,33 +2,49 @@ import sys
 import requests
 import json 
 
+import argparse
+
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
 from bs4 import BeautifulSoup
 
-def export(url, langs):
+def export(url, langs, splitline=None):
 	old_t = requests.get(url).text
 	soup = BeautifulSoup(old_t)
+	if splitline:
+		splitline = splitline[0]
 	store = soup.find(id="storeArea")
 	rows = []
+	split_stuff = []
+	split_row = 0
 	row = 0
 	for twee in store.find_all("div"):
-		if not twee.string.startswith("data:") and not "background-size" in twee.string and not "\"requires jQuery\"" in twee.string and not "(function (" in twee.string:
+		if not twee.string.startswith("data:") and not "background-size" in twee.string and not "\"requires jQuery\"" in twee.string and not "(function (" in twee.string and not "#storyTitle" in twee.string:
 			#t = t.replace(twee.string, "twine_trans(%s);" % str(row))
 			tw = twee.string
+			if splitline in tw:
+				stw = tw.split(splitline)
+				try:
+					i = split_stuff.index(stw[1])
+					twee["data-split"] = i
+				except:
+					split_stuff.append(stw[1])
+					twee["data-split"] = split_row
+					split_row += 1
+				rows.append(stw[0])
+			else:
+				rows.append(tw)
 			twee["data-trans"] = "twine_trans(%s);" % str(row)
-			#twee.string = "twine_trans(%s);" % str(row)
-			rows.append(tw)
+
 			row += 1
 	t = soup.prettify();
-
 
 	j_script = open("jquery.min.js").read()
 	t_script = open("translate.js").read()
 	t = t.replace(
 		"</title>", 
-		"</title>\n<script title=\"jquery\">\n\n"+j_script+"\n\n</script>\n<script title=\"translation\">\n\n//magic\n\nvar langs="+json.dumps(langs)+";\n\n"+t_script+"\n\n</script>"
+		"</title>\n<script title=\"jquery\">\n\n"+j_script+"\n\n</script>\n<script title=\"translation\">\n\n//magic\n\nvar langs="+json.dumps(langs)+"; var splitline="+str(row)+";\n\n"+t_script+"\n\n</script>"
 	)
 
 	file_name = url.split("/")[-1]	
@@ -39,9 +55,13 @@ def export(url, langs):
 	origin.append(file_name.split(".")[-1])
 	with open(".".join(origin), "wb") as h: 
 		h.write(old_t)
+
+	translateable = "\n".join(rows)
+	if splitline:
+		translateable += "\n" + splitline + "\n" + "\n".join(split_stuff)
 	for lang in langs:
 		with open(lang+".txt", "wb") as h:
-			h.write("\n".join(rows))
+			h.write(translateable)
 
 	return ".".join(origin), file_name
 
@@ -49,7 +69,6 @@ import zipfile
 import uuid
 
 def compress(origin, file, langs):
-
 	mongooses = ["mongoose-linux", "mongoose-osx.dmg", "mongoose-win.exe"]
 	zname = str(uuid.uuid4())+".zip"
 	with zipfile.ZipFile(zname, 'w', zipfile.ZIP_DEFLATED) as myzip:
@@ -62,5 +81,21 @@ def compress(origin, file, langs):
 	return zname
 	
 if __name__ == "__main__":
-	origin, file_name = export (sys.argv[1], sys.argv[2:])
-	print compress(origin, file_name, sys.argv[2:])
+	parser = argparse.ArgumentParser(description='Twine Translation Extractor')
+	parser.add_argument("url", metavar="URL", nargs=1, action="store", help="The url of the twine game to translate")
+	parser.add_argument("langs", metavar="LANG", nargs="+", action='store', help="The target languages of the translation")
+	
+	parser.add_argument("--fromlang", metavar="LANG", nargs="?", default="eng", action='store', help="The starting language of the translation")
+
+	parser.add_argument("--splitline", metavar="SYMBOLS", nargs=1, action="store", help="if a certain part of the line is repeated, you might want to have it in a separate part of the translation file. ")
+
+	args = parser.parse_args()
+	langs_list = [args.fromlang]
+	for lang in args.langs:
+		langs_list.append(lang)
+	print args.splitline
+	origin, file_name = export (args.url[0], langs_list, args.splitline)
+	print compress(origin, file_name, args.langs)
+
+	
+	
--- translate.js
+++ translate.js
@@ -10,12 +10,18 @@ jQuery(function(){
 		jQuery.get(lang+".txt", function(data){
 			__data = data.split("\n");
 			jQuery("div#storeArea").find("div").each(function(e,i){
-				msg = jQuery(this).data("trans");
-				if (msg.indexOf("twine_trans")>=0){
-					try{
-						msg = eval(msg);
-					} catch (e){}
-					jQuery(this).text(msg);
+				var msg = jQuery(this).data("trans");
+				var splat = jQuery(this).data("split");
+				if (msg !== undefined){
+					if (msg.indexOf("twine_trans")>=0){
+						try{
+							msg = eval(msg);
+						} catch (e){}
+						if (splat !== undefined){
+							msg = msg + __data[splitline] + __data[splitline+1+splat]
+						}
+						jQuery(this).text(msg);
+					}
 				}
 			});
 		});
@@ -28,14 +34,15 @@ jQuery(function(){
 
 	load_lang(langs[0]);
 
-	var appendable = jQuery("<li id='lang'><select id='t_select'></select></li>");
-	for (var i in langs){
-		appendable.find("#t_select").append("<option value='"+langs[i]+"'>"+langs[i]+"</option>");
+	if (langs.length>1){
+		var appendable = jQuery("<li id='lang'><select id='t_select'></select></li>");
+		for (var i in langs){
+			appendable.find("#t_select").append("<option value='"+langs[i]+"'>"+langs[i]+"</option>");
+		}
+		jQuery("#sidebar").append(appendable);
+		jQuery(window.document).on("change", "#t_select", function(){
+			load_lang(jQuery("#t_select").val());
+		});
 	}
-	jQuery("#sidebar").append(appendable);
-
-	jQuery(window.document).on("change", "#t_select", function(){
-		load_lang(jQuery("#t_select").val());
-	});
 	
 });
